7095bbf65d11bd4397af87a24dca9b3b5db4dec2
Gauthier robin <91669428+GauthierRobin@users.noreply.github.com>
2022-09-08 20:08:56 +0200

    single page

--- js/class/game.js
+++ js/class/game.js
@@ -1,82 +1,74 @@
 import { Player } from "/js/class/player.js";
 import { Ui } from "/js/class/ui.js";
-import { Animate } from "/js/class/animation.js";
+import { Client } from "./client.js";
+import { Sound } from "./sound.js";
+import { Animation } from "./animation.js"
+import { Loader } from "./loader.js";
 
 export class Game {
     constructor() {
-        this.player1 = new Player(this, document.getElementById("grille2"), 2)
-        this.player2 = new Player(this, document.getElementById("grille1"), 1, true)
+        this.player1
+        this.player2
+        this.roomId
         this.ui = new Ui(this)
-        this.run = true
-        this.anim = null;
+        this.sound = new Sound()
+        this.animation = new Animation()
+        this.loader = new Loader()
+        this.client = new Client(this, true) // true for local serveur, false for heroku serveur
     }
 
     init() {
-        document.getElementById('namePlayer1').innerText = this.player1.name;
-        document.getElementById('namePlayer2').innerText = this.player2.name;
+        this.player1 = new Player(this, document.getElementById("grille2"), 2)
+        this.player2 = new Player(this, document.getElementById("grille1"), 1)
 
-        this.player1.grid = [null, null, null, null, null, null, null, null, null]
+        this.client.init()
+
         this.player1.otherPlayer = this.player2
-        this.player1.drawGrille()
-        this.player1.turn = true;
-
-        this.player2.grid = [null, null, null, null, null, null, null, null, null]
         this.player2.otherPlayer = this.player1
-        this.player2.drawGrille()
 
-        console.log(`Player ${this.player1.id}:${this.player1.turn}`);
-        console.log(`Player ${this.player2.id}:${this.player2.turn}`);
+        this.ui.init()
+        this.player1.initControl()
+    }
+
+    play(mode) {
+        this.player1.setName()
+        this.sound.play('bgAudio', 0.01, true)
+        this.sound.play('SeaSound', 0.01, true)
+
+        switch (mode) {
+            case "solo":
+                this.client.joinSolo()
+                this.show()
+                break;
+
+            case "multi":
+                $("#index").fadeOut(400)
+                this.loader.add("En attente d'un autre joueur ...")
+                this.client.joinMulti()
+                break;
+
+            default: this.client.joinSolo(); break;
+        }
+    }
+
+    show() {
+        $("#index").fadeOut(400)
+        $("#game").fadeIn(400)
     }
 
     reset() {
         this.ui.restartGame()
-        this.player1.reset()
-        this.player1.turn = true
-        this.player2.reset()
-        this.run = true
-    }
-
-    destroyEnemieDices(gamePLayer, otherPlayer) {
-        let gridPlayer1 = gamePLayer.getFormatGrid()
-        let gridPlayer2 = otherPlayer.getFormatGrid()
-        let newGrid = []
-
-
-        this.anim = new Animate(gamePLayer, otherPlayer);
-        for (let nbCol = 0; nbCol < gridPlayer1.length; nbCol++) {
-
-            gridPlayer2[nbCol].forEach((element) => {
-                if (gridPlayer1[nbCol].includes(element) && element !== null) {
-                    this.anim.animate(document.getElementById(`player${otherPlayer.id}-col${nbCol + 1}-case-${gridPlayer2[nbCol].indexOf(element) + 1}`));
-                    newGrid.push(null)
-                } else {
-                    newGrid.push(element)
-                }
-            });
-        }
-        otherPlayer.grid = newGrid
-        otherPlayer.setSortedGrid()
-    }
-
-    getTotalCase() {
-        return this.player1.nbCol * this.nbColCase
     }
 
     switchTurn() {
-        this.player1.turn = !this.player1.turn
-        this.player2.turn = !this.player2.turn
-
-        if (this.player1.gridIsFull() || this.player2.gridIsFull()) {
-            this.ui.endGame()
-            this.run = false
+        if (this.player1.turn) {
+            document.getElementById('potPlayer1').setAttribute('class', 'choice-dice clignote')
+            document.getElementById('potPlayer2').setAttribute('class', 'choice-dice')
         }
 
-        if (this.player1.turn && this.player1.ai && this.run) {
-            this.player1.aiTurn()
-        }
-
-        if (this.player2.turn && this.player2.ai && this.run) {
-            this.player2.aiTurn()
+        if (this.player2.turn) {
+            document.getElementById('potPlayer1').setAttribute('class', 'choice-dice')
+            document.getElementById('potPlayer2').setAttribute('class', 'choice-dice clignote')
         }
     }
 } 
